5b4a0d5db3985772d830d2e1aa3d0f3a955b6d16
Martin93-css <tin.deunaa@gmail.com>
2023-04-25 20:47:59 -0300

    terminando con tablas de shows y arreglando errores en genres y ultimo shows

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,64 +1,81 @@
-import React from 'react';
-import ChartRow from './ChartRow';
+import React, { Component } from 'react'
 
-let tableRowsData = [
-    {
-        Title: 'Billy Elliot ',
-        Length: '123',
-        Rating: '5',
-        Categories: ['Drama','Comedia'],
-        Awards: 2
-    },
-    {
-        Title: 'Alicia en el país de las maravillas',
-        Length: '142',
-        Rating: '4.8',
-        Categories: ['Drama','Acción','Comedia'],
-        Awards: 3
-    },
+import ChartRow from "./ChartRow";
+
+
+
+
+export class Chart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          ShowsList: [], // Traer todos los shows
+        };
+      }
     
-]
-
-
-function Chart (){
-    return (
-        /* <!-- DataTales Example --> */
-        <div className="card shadow mb-4">
-            <div className="card-body">
-                <div className="table-responsive">
-                    <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
-                        <thead>
-                            <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            {
-                            tableRowsData.map( ( row , i) => {
-                                return <ChartRow { ...row} key={i}/>
-                            })
-                            }
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+      getAllShows() {
+        fetch("http://localhost:3005/api/shows")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            this.setState({
+              ShowsList: data.data, // actualizar a showslist
+            });
+          })
+          .catch((error) => console.log(error));
+      }
+    
+      componentDidMount() {
+        this.getAllShows();
+      }
+  
+  
+  
+    render() {
+    
+        return (
+            <React.Fragment>
+    <div className="card shadow mb-4">
+      <div className="card-body">
+        <div className="table-responsive">
+          <table
+            className="table table-bordered"
+            id="dataTable"
+            width="100%"
+            cellSpacing="0"
+          >
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Género</th>
+                <th>Precio</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Título</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Género</th>
+                <th>Precio</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              {this.state.ShowsList.map((row, i) => {
+                return <ChartRow {...row} key={i} />;
+              })}
+            </tbody>
+          </table>
         </div>
-
-    )
+      </div>
+    </div>
+    </React.Fragment>)
+ 
+  }
 }
 
-export default Chart;
+export default Chart
+
+
--- src/components/ChartRow.js
+++ src/components/ChartRow.js
@@ -4,17 +4,11 @@ import React from 'react';
 function ChartRow(props){
     return (
                 <tr>
-                    <td>{props.Title}</td>
-                    <td>{props.Length}</td>
-                    <td>{props.Rating}</td>
-                    <td>
-                        <ul>
-                            {props.Categories.map( (category,i) => 
-                                <li key={`category ${i}`}>{category}</li>
-                            )}
-                        </ul>
-                    </td>
-                    <td>{props.Awards}</td>
+                    <td>{props.name}</td>
+                    <td>{props.date}</td>
+                    <td>{props.hour}</td>
+                    <td>{props.Category.name}</td>
+                    <td>{props.price}</td>
                 </tr>
             )
     }
